fcba97e83a90ea49ec2982c66bd633bca18d51c9
EmilUpp <emil.sitell@gmail.com>
2020-10-22 15:21:17 +0200

    Reading form csv

--- reading_csv.py
+++ reading_csv.py
@@ -1,15 +1,26 @@
 import csv
 
-with open("test.csv", "r") as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=",")
+def read_data_from_file(filepath):
+    with open(filepath, "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=",")
 
-    cleanList = list()
+        cleanList = list()
 
-    row_count = 0
-    for row in csv_reader:
-        if (row["heartRate"] != 0):
-            cleanList.append((row_count, row["heartRate"]))
+        row_count = 0
+        for row in csv_reader:
+            cleanList.append([row_count, row["heartRate"]])
+            row_count += 1
 
-        row_count += 1
+    return cleanList
 
-print(cleanList)
+def write_to_file(dataTuple_to_write, filepath):
+    with open(filepath, "w+", newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=",")
+        csv_writer.writerow(['Index', 'Heartrate'])
+
+        for each in dataTuple_to_write:
+            csv_writer.writerows([each])
+
+data = read_data_from_file("test.csv")
+
+write_to_file(data, "clean_file.csv")
